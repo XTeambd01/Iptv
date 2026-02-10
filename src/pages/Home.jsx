ae1de29e299fffe1726d3d4c7cecf55ea6b72657
Sany X nub <143182537+XTeambd01@users.noreply.github.com>
2026-02-10 19:18:15 +0600

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,1 @@
+export default function Home(){return(<div style={{width:"100vw",height:"100vh"}}><iframe src="/demo.html" style={{width:"100%",height:"100%",border:"none"}} title="Sportzft"/></div>);}
